bed61d88c33c5e37efa6308d7177c67c7d83bdc7
Kevin Kim <kebonkim@gmail.com>
2024-01-10 23:24:10 -0800

    Updates

---
+++ precache-manifest.afd3200308a60d7240891073dd4b7749.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03957d4c27b90716a9b0401bdc864917",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0415ab7ebc3c2f5a351",
+    "url": "/static/css/main.5b26f059.chunk.css"
+  },
+  {
+    "revision": "4d665d15a336a088dc0e",
+    "url": "/static/js/2.cc2478fb.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "e0415ab7ebc3c2f5a351",
+    "url": "/static/js/main.56ca0806.chunk.js"
+  },
+  {
+    "revision": "2bd38feb6fb022a94fd0",
+    "url": "/static/js/runtime~main.cd66bee5.js"
+  },
+  {
+    "revision": "273301548cc25db4dce013244ec56902",
+    "url": "/static/media/airbb.27330154.png"
+  },
+  {
+    "revision": "22f1a501bcbe2752a2eb48d0d32f8b3a",
+    "url": "/static/media/artsy.22f1a501.png"
+  },
+  {
+    "revision": "da222d294a53c32ec01918904db82431",
+    "url": "/static/media/avatar.da222d29.png"
+  },
+  {
+    "revision": "8f042c7d6672dd19c998883fb2bf3ef0",
+    "url": "/static/media/bg.8f042c7d.jpg"
+  },
+  {
+    "revision": "b02e163f818e8e4c86b403ae86adda0f",
+    "url": "/static/media/pic01.b02e163f.jpg"
+  },
+  {
+    "revision": "06c70c53f5c1a2cdb7feb483760774df",
+    "url": "/static/media/wintertrails.06c70c53.png"
+  }
+]);
