3fc35b49a701361d1a69b7b3972b598a8461c0e1
Kevin Kim <kebonkim@gmail.com>
2024-04-16 17:05:27 -0700

    Update Welcome

--- src/components/WelcomeSign/index.tsx
+++ src/components/WelcomeSign/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import './WelcomeSign.css'
 import ProfileButton from "../ProfileButton";
 
-import avatarImg from '../../assets/images/avatar.png'
 
 interface ImageProps {
   src: string;
@@ -14,31 +13,34 @@ interface ImageProps {
 
 
 
-const WelcomeSign : React.FC<ImageProps> = ({src, alt, profileimg, width, height}) => {
+const WelcomeSign : React.FC<ImageProps> = ({src, alt}) => {
     return (
         <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
             <ProfileButton />
             <img className="welcome_image" src={src} alt={alt}/>
-            {/* <div style={{width: "100%", height:'500px'}}></div> */}
             <div className="welcome_text">
                 <div className="profile_image_div">
-                    <div style={{display:'flex'}}>
+                    <div style={{display:'flex', width: '600px', flexDirection: 'column'}}>
+                        <div style={{display:'flex', flexDirection: 'row'}}>
                         <div className="hi">H</div>
-                        <div className="hi">i</div>
-                        <div>&nbsp;</div>
+                        <div className="hi">i,</div>
+                        </div>
+                        <div style={{display: 'flex', flexDirection: 'row'}}>
                         <div className="hi">I</div>
                         <div className="hi">'</div>
-                        <div className="hi">m</div>
-                        <div>&nbsp;</div>
+                        <div className="hi">m&nbsp;</div>
                         <div className="hi">K</div>
                         <div className="hi">e</div>
                         <div className="hi">v</div>
                         <div className="hi">i</div>
-                        <div className="hi">n</div>
+                        <div className="hi">n&nbsp;</div>
+                         <div className="hi">K</div>
+                        <div className="hi">i</div>
+                        <div className="hi">m</div>
+                        </div>
                         <div>&nbsp;</div>
-                        <div style={{fontSize:'16px'}} className="hi">👋</div>
                     </div>
-                <img className="profile_image" src={avatarImg} alt="avatar"/>
+                {/* <img className="profile_image" src={avatarImg} alt="avatar"/> */}
                     <div className="profile_con_text">
                     <div id="console">console<span id="period">.</span><span id="log">log</span><span className="parenthis">(</span>
                     </div>
@@ -47,15 +49,9 @@ const WelcomeSign : React.FC<ImageProps> = ({src, alt, profileimg, width, height
                 <div className="profile_text_div">
                     <div className="profile_inner">"Software Engineer"</div>
                     <div className="profile_inner">"Frontend Engineer"</div>
-                    <div className="profile_inner">"Nature Enthusiast"</div>
-                    <div className="profile_inner">"Problem-Solver"</div>
+                    <div className="profile_inner">"Fullstack Engineer"</div>
                 </div>
-
                 </div>
-            {/* <div>
-            <div className="">Software Engineer</div>
-            <div></div>
-            </div> */}
             </div>
         </div>
     )
