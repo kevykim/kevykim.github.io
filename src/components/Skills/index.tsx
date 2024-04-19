fe12b06d9dd4acbf60d9d6eca11f8f664cfb976e
Kevin Kim <kebonkim@gmail.com>
2024-04-18 23:37:21 -0700

    added links to skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,38 +1,239 @@
 import React from "react";
-import './skills.css'
-
+import "./skills.css";
 
 const Skills: React.FC = () => {
+  return (
+    <div id="skills">
+      <div className="skills_title">Skills</div>
+      <div className="skills_icons">
+        <a
+          href={"https://developer.mozilla.org/en-US/docs/Web/JavaScript"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=js"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.typescriptlang.org/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=ts"
+            alt="skills"
+          />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://react.dev/">
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=react"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://redux.js.org/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=redux"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=html"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=css"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://tailwindcss.com/"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=tailwind"
+            alt="skills"
+          />
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://nextjs.org/">
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=nextjs"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.figma.com/"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=figma"
+            alt="skills"
+          />
+        </a>
 
-    return (
-        <div id="skills">
-            <div className="skills_title">
-                Skills
-            </div>
-            <div className="skills_icons">
-			<img className="skills_image" src="https://skillicons.dev/icons?i=js" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=ts" alt="skills"/>
-			<img className="skills_image" src="https://skillicons.dev/icons?i=react" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=redux" alt="skills"/>
-			<img className="skills_image" src="https://skillicons.dev/icons?i=html" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=css" alt="skills"/>
-			<img className="skills_image" src="https://skillicons.dev/icons?i=py" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=flask" alt="skills"/>
-			<img className="skills_image" src="https://skillicons.dev/icons?i=express" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=postgres" alt="skills"/>
-			<img className="skills_image" src="https://skillicons.dev/icons?i=sqlite" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=nodejs" alt="skills"/>
-			<img className="skills_image" src="https://skillicons.dev/icons?i=heroku" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=git" alt="skills"/>
-			<img className="skills_image" src="https://skillicons.dev/icons?i=docker" alt="skills"/>
-			<img className="skills_image_1" src="https://skillicons.dev/icons?i=aws" alt="skills"/>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.python.org/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=py"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://flask.palletsprojects.com/en/3.0.x/"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=flask"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://expressjs.com/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=express"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://nodejs.org/en"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=nodejs"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.postgresql.org/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=postgres"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://sequelize.org/"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=sequelize"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.sqlite.org/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=sqlite"
+            alt="skills"
+          />
+        </a>
 
-            </div>
-
-
-        </div>
-    )
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.heroku.com/"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=heroku"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://git-scm.com/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=git"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.postman.com/"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=postman"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.docker.com/"
+        >
+          <img
+            className="skills_image_1"
+            src="https://skillicons.dev/icons?i=docker"
+            alt="skills"
+          />
+        </a>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://aws.amazon.com/"
+        >
+          <img
+            className="skills_image"
+            src="https://skillicons.dev/icons?i=aws"
+            alt="skills"
+          />
+        </a>
+      </div>
+    </div>
+  );
 };
 
-
-export default Skills
+export default Skills;
